3cec9446a1e81687aa18a32bd66f8b86d02d6f50
Joey Santia <joey.santia@gmail.com>
2022-12-28 23:14:05 -0500

    Import images, update fonts

--- src/App.js
+++ src/App.js
@@ -3,107 +3,127 @@ import Header from './components/Header'
 import Card from './components/Card'
 import Counter from './components/Counter'
 import HighScore from './components/HighScore'
+import mordecaiImg from './img/mordecai.webp'
+import rigbyImg from './img/rigby.webp'
+import muscleManImg from './img/muscle-man.webp'
+import bensonImg from './img/benson.webp'
+import skipsImg from './img/skips.webp'
+import popsImg from './img/pops.webp'
+import highFiveGhostImg from './img/high-five-ghost.webp'
+import eileenImg from './img/eileen.webp'
+import margaretImg from './img/margaret.webp'
+import CJImg from './img/cj.webp'
+import thomasImg from './img/thomas.webp'
+import maellardImg from './img/maellard.webp'
+import starlaImg from './img/starla.webp'
+import garyImg from './img/gary.webp'
+import guardianImg from './img/guardian.webp'
+import deathImg from './img/death.webp'
+import lowFiveGhostImg from './img/low-five-ghost.webp'
+import GBFImg from './img/gbf.webp'
+import donImg from './img/don.webp'
+import muscleBroImg from './img/muscle-bro.webp'
 import './App.css';
 
 function App() {
   const [cardArray, setCardArray] = useState([
     {id: 'Mordecai',
-     img: '../src/img/mordecai.webp',
+     img: mordecaiImg,
      index: 0,
      hasBeenClicked: false 
     },
     {id: 'Rigby',
-     img: '#',
+     img: rigbyImg,
      index: 1,
      hasBeenClicked: false 
     },
     {id: 'Muscle Man',
-     img: '#',
+     img: muscleManImg,
      index: 2,
      hasBeenClicked: false 
     },
     {id: 'Benson',
-     img: '#',
+     img: bensonImg,
      index: 3,
      hasBeenClicked: false 
     },
     {id: 'Skips',
-     img: '#',
+     img: skipsImg,
      index: 4,
      hasBeenClicked: false 
     },
     {id: 'Pops',
-     img: '#',
+     img: popsImg,
      index: 5,
      hasBeenClicked: false 
     },
     {id: 'High Five Ghost',
-     img: '#',
+     img: highFiveGhostImg,
      index: 6,
      hasBeenClicked: false 
     },
     {id: 'Thomas',
-     img: '#',
+     img: thomasImg,
      index: 7,
      hasBeenClicked: false 
     },
     {id: 'Eileen',
-     img: '#',
+     img: eileenImg,
      index: 8,
      hasBeenClicked: false 
     },
     {id: 'Margaret',
-     img: '#',
+     img: margaretImg,
      index: 9,
      hasBeenClicked: false 
     },
     {id: 'Guardian of Eternal Youth',
-     img: '#',
+     img: guardianImg,
      index: 10,
      hasBeenClicked: false 
     },
     {id: 'Gary',
-     img: '#',
+     img: garyImg,
      index: 11,
      hasBeenClicked: false 
     },
     {id: 'Don',
-     img: '#',
+     img: donImg,
      index: 12,
      hasBeenClicked: false 
     },
     {id: 'Mr. Maellard',
-     img: '#',
+     img: maellardImg,
      index: 13,
      hasBeenClicked: false 
     },
     {id: 'Starla',
-     img: '#',
+     img: starlaImg,
      index: 14,
      hasBeenClicked: false 
     },
     {id: 'Death',
-     img: '#',
+     img: deathImg,
      index: 15,
      hasBeenClicked: false 
     },
     {id: 'Muscle Bro',
-     img: '#',
+     img: muscleBroImg,
      index: 16,
      hasBeenClicked: false 
     },
     {id: 'Low Five Ghost',
-     img: '#',
+     img: lowFiveGhostImg,
      index: 17,
      hasBeenClicked: false 
     },
     {id: 'Garrett Bobby Ferguson',
-     img: '#',
+     img: GBFImg,
      index: 18,
      hasBeenClicked: false 
     },
     {id: 'CJ',
-     img: '#',
+     img: CJImg,
      index: 19,
      hasBeenClicked: false 
     }
